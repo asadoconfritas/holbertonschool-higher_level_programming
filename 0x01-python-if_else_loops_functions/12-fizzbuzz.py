a7c8caa72634cbf3cd7d95356a3aa919f48b0128
root <guzchelsea24@gmail.com>
2021-12-21 18:53:52 -0800

    fizzbuzz

--- 0x01-python-if_else_loops_functions/12-fizzbuzz.py
+++ 0x01-python-if_else_loops_functions/12-fizzbuzz.py
@@ -1,2 +1,14 @@
 #!/usr/bin/python3
-xd
+def fizzbuzz():
+    for number in range(100):
+        number = number + 1
+        string = ""
+        if (number % 15) == 0:
+            string += "FizzBuzz"
+        elif (number % 3) == 0:
+            string += "Fizz"
+        elif (number % 5) == 0:
+            string += "Buzz"
+        if string == "":
+            string = str(number)
+        print("{} ".format(string), end="")
