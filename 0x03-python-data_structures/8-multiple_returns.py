127650aeb7b530c3ad3a9285538d32b7bd9ec5c5
root <guzchelsea24@gmail.com>
2022-01-03 18:48:26 -0800

    5 n 8 fix

--- 0x03-python-data_structures/8-multiple_returns.py
+++ 0x03-python-data_structures/8-multiple_returns.py
@@ -1,4 +1,4 @@
 #!/usr/bin/python3
 def multiple_returns(sentence):
-    lrg = len(sentence)
-    return lrg, sentence[0] if lar > 0 else None
+    lar = len(sentence)
+    return lar, sentence[0] if lar > 0 else None
