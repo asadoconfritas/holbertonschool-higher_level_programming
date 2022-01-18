a8ea52800e9b5a1a16d111f950387ad8f7dd2e9c
root <guzchelsea24@gmail.com>
2022-01-17 18:50:16 -0800

    upd 2, 100, 101

--- 0x05-python-exceptions/100-safe_print_integer_err.py
+++ 0x05-python-exceptions/100-safe_print_integer_err.py
@@ -1,3 +1,9 @@
 #!/usr/bin/python3
 def safe_print_integer_err(value):
-    f
+    import sys
+    try:
+        print("{:d}".format(value))
+        return True
+    except Exception as exe:
+        sys.stderr.write("Exception: {}\n".format(str(exe)))
+        return False
--- 0x05-python-exceptions/2-safe_print_list_integers.py
+++ 0x05-python-exceptions/2-safe_print_list_integers.py
@@ -1,11 +1,11 @@
 #!/usr/bin/python3
 def safe_print_list_integers(my_list=[], x=0):
-    i = 0;
+    nn = 0
     for ind in range(x):
         try:
             print("{:d}".format(my_list=[ind]), end="")
-            i += 1
+            nn = nn + 1
         except (TypeError, ValueError):
             continue
     print()
-    return i
+    return nn
