554dce38f3cfe3e3821da9f686b709c5f2ba83d9
root <guzchelsea24@gmail.com>
2022-01-17 18:57:51 -0800

    hopefully i fixed 2 lol

--- 0x05-python-exceptions/2-safe_print_list_integers.py
+++ 0x05-python-exceptions/2-safe_print_list_integers.py
@@ -1,6 +1,7 @@
 #!/usr/bin/python3
 def safe_print_list_integers(my_list=[], x=0):
     nn = 0
+    ind = 0
     for ind in range(x):
         try:
             print("{:d}".format(my_list=[ind]), end="")
