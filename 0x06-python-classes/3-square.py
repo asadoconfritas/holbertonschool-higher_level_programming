03384af619869d52666ce4ec2089b7adeba6719f
root <guzchelsea24@gmail.com>
2022-01-18 17:16:29 -0800

    upd 3

--- 0x06-python-classes/3-square.py
+++ 0x06-python-classes/3-square.py
@@ -1,6 +1,18 @@
 #!/usr/bin/python3
-"""3. Area of a square"""
+"""2. Size validation """
 
 
 class Square:
-    s
+    """Square"""
+    def __init__(self, size=0):
+        """cons method"""
+        if int(size) < 0:
+            raise ValueError("size must be >= 0")
+        elif type(size) is not int:
+            raise TypeError("size must be an integer")
+        else:
+            self.__size = size
+
+    def area(self):
+        """area of the Square"""
+        return self.__size * self.__size
