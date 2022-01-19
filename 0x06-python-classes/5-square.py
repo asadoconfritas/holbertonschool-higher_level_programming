99ab1a77de453c30834ad2b9bc8819739a5683b7
root <guzchelsea24@gmail.com>
2022-01-18 17:30:49 -0800

    adding files for 5

--- 0x06-python-classes/5-square.py
+++ 0x06-python-classes/5-square.py
@@ -1,6 +1,36 @@
 #!/usr/bin/python3
-"""
-    5. Printing a square
-"""
+"""5. Printing a square"""
+
+
 class Square:
-    s
+    """Square"""
+    def __init__(self, size=0):
+        """cons method"""
+        self.__size = size
+
+    def area(self):
+        """area of the Square"""
+        return self.__size * self.__size
+
+    @property
+    def size(self):
+        """size"""
+        return self.__size
+
+    @size.setter
+    def size(self, size):
+        if type(size) is not int:
+            raise TypeError("size must be an integer")
+        elif int(size) < 0:
+            raise ValueError("size must be >= 0")
+        else:
+            self.__size = size
+
+    def my_print(self):
+        """myprint"""
+        for y in range(self.__size):
+            for x in range(self.__size):
+                print("#", end="")
+            print()
+        if not self.__size:
+            print()
