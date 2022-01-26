9628567c9a1df3922abfe33102bb7cc84ea0cafd
root <guzchelsea24@gmail.com>
2022-01-25 18:49:46 -0800

    upd advanced

---
+++ 0x09-python-everything_is_object/101-locked_class.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+"""advanced ex 101"""
+
+
+class LockedClass:
+    """lockedclass"""
+
+    __slots__ = ['first_name']
+
+    def init(self):
+        """init x"""
+        pass
