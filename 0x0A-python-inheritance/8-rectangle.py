0259086024e3cf645dbf647d65b880bf4e99e9a8
root <guzchelsea24@gmail.com>
2022-01-31 17:57:20 -0800

    rectangle

--- 0x0A-python-inheritance/8-rectangle.py
+++ 0x0A-python-inheritance/8-rectangle.py
@@ -1,5 +1,15 @@
 #!/usr/bin/python3
-""" """
+"""mod doc"""
 
 
-def 
+basegeo = __import__('7-base_geometry').BaseGeometry
+
+
+class Rectangle(basegeo):
+    """rectangle doc"""
+    def __init__(self, width, height):
+        """init doc"""
+        self.integer_validator("width", width)
+        self.integer_validator("height", height)
+        self.__width = width
+        self.__height = height
