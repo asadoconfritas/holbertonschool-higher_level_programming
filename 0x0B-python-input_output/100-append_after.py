2fa383e05e7f97ac954c8c3713c0edc251e08052
root <guzchelsea24@gmail.com>
2022-02-01 18:34:25 -0800

    update 100

--- 0x0B-python-input_output/100-append_after.py
+++ 0x0B-python-input_output/100-append_after.py
@@ -4,3 +4,11 @@
 
 def append_after(filename="", search_string="", new_string=""):
     """append after"""
+    with open(filename, 'r+', encoding='utf8') as f:
+        out = ""
+        for line in f:
+            out += line
+            if search_string in line:
+                out += new_string
+        f.seek(0)
+        f.write(out)
