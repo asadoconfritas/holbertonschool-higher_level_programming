05a22ecb2361087bdf51abf6cd1083274f8adcc0
root <guzchelsea24@gmail.com>
2022-02-08 09:23:42 -0800

    fix

--- 0x0C-python-almost_a_circle/models/rectangle.py
+++ 0x0C-python-almost_a_circle/models/rectangle.py
@@ -17,100 +17,100 @@ class Rectangle(Base):
         self.x = x
         self.y = y
 
-        @property
-        def width(self):
-            """ width """
-            return self.__width
+    @property
+    def width(self):
+        """ width """
+        return self.__width
 
-        @width.setter
-        def width(self, width):
-            """ w setter """
-            if type(width) is not int:
-                raise TypeError("width must be an integer")
-            if width <= 0:
-                raise ValueError("width must be > 0")
-            self.__width = width
+    @width.setter
+    def width(self, width):
+        """ w setter """
+        if type(width) is not int:
+            raise TypeError("width must be an integer")
+        if width <= 0:
+            raise ValueError("width must be > 0")
+        self.__width = width
 
-        @property
-        def height(self):
-            """ height """
-            return self.__height
+    @property
+    def height(self):
+        """ height """
+        return self.__height
 
-        @height.setter
-        def height(self, height):
-            """ height setter """
-            if type(height) is not int:
-                raise TypeError("height must be an integer")
-            if height <= 0:
-                raise ValueError("height must be > 0")
-            self.__height = height
+    @height.setter
+    def height(self, height):
+        """ height setter """
+        if type(height) is not int:
+            raise TypeError("height must be an integer")
+        if height <= 0:
+            raise ValueError("height must be > 0")
+        self.__height = height
 
-        @property
-        def x(self):
-            """ x get """
-            return self.__x
+    @property
+    def x(self):
+        """ x get """
+        return self.__x
 
-        @x.setter
-        def x(self, value):
-            """ x setter """
-            if type(x) is not int:
-                raise TypeError("x must be an integer")
-            if x <= 0:
-                raise ValueError("x must be > 0")
-            self.__x = x
+    @x.setter
+    def x(self, value):
+        """ x setter """
+        if type(x) is not int:
+            raise TypeError("x must be an integer")
+        if x <= 0:
+            raise ValueError("x must be > 0")
+        self.__x = x
 
-        @property
-        def y(self):
-            """ y get """
-            return self.__y
+    @property
+    def y(self):
+        """ y get """
+        return self.__y
         
-        @y.setter
-        def y(self, value):
-            """ y setter """
-            if type(y) is not int:
-                raise TypeError("y must be an integer")
-            if y <= 0:
-                raise ValueError("y must be > 0")
-            self.__y = y
+    @y.setter
+    def y(self, value):
+        """ y setter """
+        if type(y) is not int:
+            raise TypeError("y must be an integer")
+        if y <= 0:
+            raise ValueError("y must be > 0")
+        self.__y = y
 
-        def area(self):
-            """ area """
-            return self.__width * self.__height
+    def area(self):
+        """ area """
+        return self.__width * self.__height
 
-        def display(self):
-            """ display """
-            print(self.__y * '\n', end="")
-            for i in range(self.height):
-                display = ""
-                display += (self.__x * ' ')
-                display += (self.width * '#')
-                print(display)
+    def display(self):
+        """ display """
+        print(self.__y * '\n', end="")
+        for i in range(self.height):
+            display = ""
+            display += (self.__x * ' ')
+            display += (self.width * '#')
+            print(display)
 
-        def __str__(self):
-            """ self """
-            return f"[Rectangle] ({self.id}) {self.x}/{self.y} -\
+    def __str__(self):
+        """ self """
+        return f"[Rectangle] ({self.id}) {self.x}/{self.y} -\
 {self.width}/{self.height}"
 
-        def update(self, *args, **kwargs):
-            """ upd """
-            lrg = len(args)
-            if lrg >= 1:
-                setattr(self, "id", args[0])
-            if lrg >= 2:
-                setattr(self, "width", args[1])
-            if lrg >= 3:
-                setattr(self, "height", args[2])
-            if lrg >= 4:
-                setattr(self, "x", args[3])
-            if lrg >= 5:
-                setattr(self, "y", args[4])
-            for key, value in kwargs.items():
-                setattr(self, key, value)
+    def update(self, *args, **kwargs):
+        """ upd """
+        lrg = len(args)
+        if lrg >= 1:
+            setattr(self, "id", args[0])
+        if lrg >= 2:
+            setattr(self, "width", args[1])
+        if lrg >= 3:
+            setattr(self, "height", args[2])
+        if lrg >= 4:
+            setattr(self, "x", args[3])
+        if lrg >= 5:
+            setattr(self, "y", args[4])
+        for key, value in kwargs.items():
+            setattr(self, key, value)
 
-        def to_dictionary(self):
-            """ todic """
-            attr = ['x', 'y', 'id', 'height', 'width']
-            res = {}
-            for key in attr:
-                res[key] = getattr(self, key)
-            return res
+    def to_dictionary(self):
+        """ todic """
+        attr = ['x', 'y', 'id', 'height', 'width']
+        res = {}
+        for key in attr:
+            res[key] = getattr(self, key)
+        return res
