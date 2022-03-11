b82c6936647f6eb8d77134a213abcf551f092d2a
root <guzchelsea24@gmail.com>
2022-03-10 18:38:00 -0800

    fix 103

--- 0x0D-SQL_introduction/103-max_state.sql
+++ 0x0D-SQL_introduction/103-max_state.sql
@@ -1,3 +1,3 @@
 -- Temperatures #2
 -- script that displays the max temperature of each state
-SELECT city, MAX(value) AS max_temp FROM temperatures GROUP BY state ORDER BY state ASC LIMIT 3
+SELECT state, MAX(value) AS max_temp FROM temperatures GROUP BY state ORDER BY state ASC LIMIT 3
