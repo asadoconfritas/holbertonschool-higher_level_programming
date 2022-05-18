4c34c7d616ba815b89511f24dd9a601ac5c2951e
root <guzchelsea24@gmail.com>
2022-05-17 19:44:51 -0700

    new 0

--- 0x14-javascript-web_scraping/0-readme.js
+++ 0x14-javascript-web_scraping/0-readme.js
@@ -1,8 +1,8 @@
 #!/usr/bin/node
-/* reads and prints the content of a file */
+// Reads and prints the content of a file
 const fs = require('fs');
 fs.writeFile(process.argv[2], process.argv[3], err => {
-	if (err) {
-		console.log(err);
-	}
+  if (err) {
+    console.error(err);
+  }
 });
