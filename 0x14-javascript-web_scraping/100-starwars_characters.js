e3ac3a6d2865ad725e06dd99c105b30335ed6ce1
root <guzchelsea24@gmail.com>
2022-05-17 20:04:42 -0700

    adv

---
+++ 0x14-javascript-web_scraping/100-starwars_characters.js
@@ -0,0 +1,19 @@
+#!/usr/bin/node
+// Prints every character for a star wars movie
+const axios = require('axios').default;
+axios.get('https://swapi-api.hbtn.io/api/films/' + process.argv[2])
+    .then((response) => {
+        const characters = response.data.characters;
+        for (let i = 0; i < characters.length; i++) {
+            const axioschar = require('axios').default;
+            axioschar.get(characters[i])
+		.then((resp) => {
+			console.log(resp.data.name);
+		});
+	}
+    })
+    .catch((error) => {
+        console.log(error);
+    })
+    .then(() => {
+});
