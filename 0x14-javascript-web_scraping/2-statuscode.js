50f95058130a29d9c6f32f0e60803320499bc2d7
root <guzchelsea24@gmail.com>
2022-05-18 19:22:46 -0700

    fix2

--- 0x14-javascript-web_scraping/2-statuscode.js
+++ 0x14-javascript-web_scraping/2-statuscode.js
@@ -1,13 +1,13 @@
 #!/usr/bin/node
 // Writes some string to some file
-const argv = process.argv;
-const axios = require('axios').default;
+const argv = process.argv
+const axios = require('axios').default
 axios.get(argv[2])
-	.then((response) => {
-	console.log('code: ' + response.status);
-	})
-	.catch((error) => {
-		console.log('code: ' + error.response.status);
-	})
-	.then(() => {
-});
+  .then((response) => {
+    console.log('code: ' + response.status)
+  })
+  .catch((error) => {
+    console.log('code: ' + error.response.status)
+  })
+  .then(() => {
+  })
