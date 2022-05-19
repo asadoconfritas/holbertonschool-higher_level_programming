a4a43a10136acf30bdac7645c225e737a811fd54
root <guzchelsea24@gmail.com>
2022-05-18 19:42:05 -0700

    upd 6

---
+++ 0x14-javascript-web_scraping/6-completed_tasks.js
@@ -0,0 +1,20 @@
+#!/usr/bin/node
+// computes the number of tasks completed by user id
+const axios = require('axios').default
+const argv = process.argv
+const apidata = argv[2]
+
+axios.get(apidata)
+  .then(function (response) {
+    const dict = {}
+    for (const x in response.data) {
+      const user = response.data[x].userId
+      const ended = response.data[x].completed
+      if (isNaN(dict[user]) && ended) {
+        dict[user] = 1
+      } else if (ended) {
+        dict[user] += 1
+      }
+    }
+    console.log(dict)
+  })
