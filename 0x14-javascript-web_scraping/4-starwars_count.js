11b9e0134e669751dfd8384d98670c5226a85a38
root <guzchelsea24@gmail.com>
2022-05-18 19:30:51 -0700

    upd 4

---
+++ 0x14-javascript-web_scraping/4-starwars_count.js
@@ -0,0 +1,18 @@
+#!/usr/bin/node
+const argv = process.argv
+const axios = require('axios').default
+const apistr = (argv[2])
+axios.get(apistr)
+  .then(function (response) {
+    let counter = 0
+
+    for (const film in response.data.results) {
+      if (response.data.results[film].characters.includes('https://swapi-api.hbtn.io/api/people/18/')) {
+        counter = counter + 1
+      }
+    }
+    console.log(counter)
+  })
+  .catch(function (error) {
+    console.log(error.response.status)
+  })
