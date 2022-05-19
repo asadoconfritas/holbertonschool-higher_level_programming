023c41eff36c184c15c93d2d95a27fc63ec901c1
root <guzchelsea24@gmail.com>
2022-05-18 19:11:45 -0700

    indent 2

--- 0x14-javascript-web_scraping/2-statuscode.js
+++ 0x14-javascript-web_scraping/2-statuscode.js
@@ -3,11 +3,11 @@
 const argv = process.argv;
 const axios = require('axios').default;
 axios.get(argv[2])
-    .then((response) => {
-        console.log('code: ' + response.status);
-    })
-    .catch((error) => {
-        console.log('code: ' + error.response.status);
-    })
-    .then(() => {
+	.then((response) => {
+	console.log('code: ' + response.status);
+	})
+	.catch((error) => {
+		console.log('code: ' + error.response.status);
+	})
+	.then(() => {
 });
