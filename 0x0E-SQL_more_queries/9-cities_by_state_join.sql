13b201ad6786c265f4bd9fd938203c8106c917d3
root <guzchelsea24@gmail.com>
2022-06-09 19:09:42 -0700

    fix 9

--- 0x0E-SQL_more_queries/9-cities_by_state_join.sql
+++ 0x0E-SQL_more_queries/9-cities_by_state_join.sql
@@ -1,6 +1,5 @@
 -- Cities by States
 -- script that lists all cities
 SELECT cities.id, cities.name, states.name FROM cities
-LEFT JOIN states
-ON cities.state_id=states_id
-ORDER BY cities.id ASC
+RIGHT JOIN states ON states.id = cities.state_id
+ORDER BY cities.id
